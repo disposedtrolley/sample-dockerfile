057a3589e863c2a98d1034f46384f511a6241f29
James Liu <james@jamesliu.io>
2022-05-16 14:23:07 +1000

    Nest sources under main/

---
+++ main/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"strings"
+)
+
+const startupMessage = "Starting main..."
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello from main! you've requested %s\n", r.URL.Path)
+	})
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "80"
+	}
+
+	for _, line := range strings.Split(startupMessage, "\n") {
+		fmt.Println(line)
+	}
+	fmt.Printf("Server listening at :%s 🚀\n", port)
+
+	err := http.ListenAndServe(fmt.Sprintf(":%s", port), nil)
+	if err != nil {
+		panic(err)
+	}
+}
